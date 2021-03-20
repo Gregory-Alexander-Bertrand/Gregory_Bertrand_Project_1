6e8b552292e5c9b67b191da0bf571969282c33bb
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-03-20 13:36:31 -0500

    third commit

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
 //console.log('test12321')
 const playerCells = Array.from(document.getElementsByClassName('cell-player'));
 const computerCells = Array.from(document.getElementsByClassName('cell'));
-let playerNavy
-let computerNavy 
+//Class constructor to make ships for fleets of computer and player
+class Ship {
+    constructor(name, length){
+        this.name = name;
+        this.length = length;
+    }
+}
+//I'm trying to make a class for the player and computer to store the ships
+//From the class above
+// class Fleets extends Ship {
+//     launchFleet(Ship){
+//         for(i = 0; i < ships.length; i++) {
+//             console.log(ships)
+//         }
+//     }
+// }
 
-console.log(computerCells)
-console.log(playerCells)
+class Fleet {
+    constructor(detail, size){
+        this.detail = [{'name': 'aircraft carrier', 'lenght': 5},
+                        {'name': 'battleship', 'length': 4},
+                        {'name': 'destroyer', 'length': 3},
+                        { 'name': 'cruiser', 'length': 2}];
+        this.size = this.detail.length;
+    }
+}
+
+console.log(Fleet)
+
+let playerFleet = new Fleet()
+let computerFleet = new Fleet()
+console.log(playerFleet)
+console.log(computerFleet)
+//Objects for ships
+let shipOne = new Ship('aircraft carrier', 5);
+let shipTwo = new Ship('battleship', 4);
+let shipThree = new Ship('destroyer', 3)
+let shipFour = new Ship('cruiser', 2)
+//Array of ships.
+//let ships = [shipOne, shipTwo, shipThree, shipFour]
+ console.log(shipOne)
+ console.log(shipTwo)
+ console.log(shipThree)
+ console.log(shipFour)
+ //console.log(ships)
 
 
 //The player can select any of the squares on the computer's board.
